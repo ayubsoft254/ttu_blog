b90834e6dc7a097ad4209fb3e9b9031bd3a61b9d
Ayubsoft254 <henryoayub15@gmail.com>
2024-07-18 12:53:04 +0300

    chore: Add posts app with models, views, and URLs

--- src/home/settings.py
+++ src/home/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'landing',
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -119,12 +120,15 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
 STATICFILES_BASE_DIR.mkdir(exist_ok=True, parents=True)
-STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "media"
 
 # source(s) for python manage.py collectstatic 
 STATICFILES_DIRS = [
     STATICFILES_BASE_DIR
 ]
+
+MEDIA_URL =  '/media/'
+MEDIA_ROOT = 'media'
 
 # output for python manage.py collectstatic 
 # local cdn
--- src/landing/urls.py
+++ src/landing/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import home_view, write_story
+from posts.views import story, opinion
 
 urlpatterns = [
     path('', home_view, name="home_view"),
-    path('story/', write_story, name='story')
+    path('story/', write_story, name='story'),
+    path('stories/', story, name='stories'),
+    path('opinions/', opinion, name='opinions'),
+
 ]
