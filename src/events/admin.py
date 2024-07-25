d018d9d0deb030ef110fd2e39685bf1a04869c04
Makai <danielmakai@gmail.com>
2024-07-25 14:03:12 +0300

    Modified Story model to include comments and likes relationships

--- src/events/admin.py
+++ src/events/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Event, UserProfile, EventCategory, Comment, Rating, Bookmark, Attendance
 
 # Register your models here.
+admin.site.register(Event)
+admin.site.register(UserProfile)
+admin.site.register(EventCategory)
+admin.site.register(Comment)
+admin.site.register(Rating)
+admin.site.register(Bookmark)
+admin.site.register(Attendance)
--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Story, Opinion
+from .models import Category, Tag, Story, Comment, Opinion, Like
 
 # Register your models here.
 admin.site.register(Story)
 admin.site.register(Opinion)
+admin.site.register(Like)
